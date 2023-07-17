c6fcf747a93f4560f333000e89ee2cfa9b07f07e
Yilun <45129420+a7959067@users.noreply.github.com>
2023-07-17 22:20:11 +0800

    Update 53 民視新聞台.php

--- php/53 民視新聞台.php
+++ php/53 民視新聞台.php
@@ -4,6 +4,6 @@ $expires=$_GET['expires'];
 $token1=$_GET['token1'];
 $expires1=$_GET['expires1'];
 
-$url="https://4gtvfreepc-cds.cdn.hinet.net/live/pool/litv-ftv13/4gtv-live-mid/index.m3u8?token=".$token."&expires=".$expires."&token1=".$token1."&expires1".$expires1.;
+$url="https://4gtvfreepc-cds.cdn.hinet.net/live/pool/litv-ftv13/4gtv-live-mid/index.m3u8?token="$token"&expires="$expires"&token1="$token1"&expires1"$expires1";
 header('location:'.urldecode($url));
 ?>
