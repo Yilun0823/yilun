8edce8ad72a97b816a324721ba83f21fe608b905
Yilun <45129420+a7959067@users.noreply.github.com>
2023-07-17 22:17:24 +0800

    Create 53 民視新聞台.php

---
+++ php/53 民視新聞台.php
@@ -0,0 +1,9 @@
+<?php
+$token=$_GET['token'];
+$expires=$_GET['expires'];
+$token1=$_GET['token1'];
+$expires1=$_GET['expires1'];
+
+$url="https://4gtvfreepc-cds.cdn.hinet.net/live/pool/litv-ftv13/4gtv-live-mid/index.m3u8?token=".$token."&expires=".$expires."&token1=".$token1."&expires1".$expires1.;
+header('location:'.urldecode($url));
+?>
