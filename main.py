1edc027749e7fedbb7d500fb461f27be31e384c6
Yilun <45129420+a7959067@users.noreply.github.com>
2023-07-23 22:31:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import json
 import requests
 
-def get_m3u8_url(channel_id):
-    url = "https://m.litv.tv/api/urls"
-    data = {
-        "AssetId": channel_id,
-        "MediaType": "channel",
-        "puid": "aa1593a5-71e6-4820-a830-72f18e5206ec"
-    }
+url = "https://m.litv.tv/api/urls"
+data = {
+    "AssetId": "4gtv-4gtv040",
+    "MediaType": "channel",
+    "puid": "aa1593a5-71e6-4820-a830-72f18e5206ec"
+}
 
-    # 添加 Cookie 到請求中 (請根據你的 Cookie 資訊填寫)
-    cookies = {
+# 添加 Cookie 到請求中 (請根據你的 Cookie 資訊填寫)
+cookies = {
     "PUID": "1bc32f7d-94b6-4570-8f38-31e08dafe055",
     "_gcl_au": "1.1.184968446.1689974529",
     "_gid": "GA1.2.1454316267.1689974529",
@@ -27,64 +25,26 @@ def get_m3u8_url(channel_id):
     "_gat_gtag_UA_145078221_1": "1",
     "_ga_73WWD84D3P": "GS1.1.1689984923.2.1.1689985877.32.0.0",
     "_ga_F72R6P7EZK": "GS1.1.1689984958.2.1.1689985883.55.0.0"
-    }
+}
+response = requests.post(url, json=data, cookies=cookies)
 
-    response = requests.post(url, json=data, cookies=cookies)
+if response.status_code == 200:
+    # 請求成功，處理回應資料
+    result = response.json()["result"]
+    asset_urls = result["AssetURLs"]
+    if len(asset_urls) > 0:
+        m3u8_url_with_params = asset_urls[0]
+        # 使用 split() 函數分割字串，只取得 .m3u8 檔案本身的網址
+        m3u8_url = m3u8_url_with_params.split("?")[0]
+        # 印出 .m3u8 檔案的網址
+        print("中視,", m3u8_url)
 
-    if response.status_code == 200:
-        # 請求成功，處理回應資料
-        try:
-            result = response.json().get("result", {})
-            asset_urls = result.get("AssetURLs", [])
-            if len(asset_urls) > 0:
-                m3u8_url_with_params = asset_urls[0]
-                # 使用 split() 函數分割字串，只取得 .m3u8 檔案本身的網址
-                m3u8_url = m3u8_url_with_params.split("?")[0]
-                return m3u8_url
-            else:
-                return None
-        except Exception as e:
-            print("解析回應資料時發生錯誤：", e)
-            return None
+        # 將 .m3u8 檔案的網址寫入 litv.txt 檔案中
+        with open("litv.txt", "w") as file:
+            file.write(f"中視, {m3u8_url}")
     else:
-        # 請求失敗，處理錯誤訊息
-        print("請求失敗，狀態碼：", response.status_code)
-        print("錯誤訊息：", response.text)
-        return None
-
-def remove_lines_after(file_path, line_number):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-
-    if line_number < 0 or line_number >= len(lines):
-        return
-
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.writelines(lines[:line_number+1])
-
-# 從 channels.json 讀取頻道資料
-with open("channels.json", 'r', encoding='utf-8') as f:
-    channels = json.loads(f.read())
-
-# 變數來存儲 tv.txt 和 tv.m3u 的內容
-txt_content = "LITV頻道,#genre#\n"
-m3u_content = "#EXTM3U\n"
-
-for channel_data in channels:
-    asset_id = channel_data["id"]
-    m3u8_url = get_m3u8_url(asset_id)
-    if m3u8_url:
-        title = channel_data["title"]
-        
-        # 添加到 tv.txt 的內容
-        txt_content += title + ',' + m3u8_url + '\n'
-        
-        # 添加到 tv.m3u 的內容
-        m3u_content += f'#EXTINF:-1 tvg-logo="{title}" tvg-name="{title}" group-title="LITV頻道",{title}\n{m3u8_url}\n'
-# 寫入 tv.txt
-with open("tv.txt", 'w', encoding='utf-8') as f:
-    f.write(txt_content)
-
-# 寫入 tv.m3u
-with open("tv.m3u", 'w', encoding='utf-8') as f:
-    f.write(m3u_content)
+        print("沒有找到 .m3u8 檔案的網址")
+else:
+    # 請求失敗，處理錯誤訊息
+    print("請求失敗，狀態碼：", response.status_code)
+    print("錯誤訊息：", response.text)
