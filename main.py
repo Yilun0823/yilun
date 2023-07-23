53493bfb9b5b51724baf4600c5ecb170ec310ccf
Yilun <45129420+a7959067@users.noreply.github.com>
2023-07-23 18:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def get_m3u8_url(channel_id):
     if response.status_code == 200:
         # 請求成功，處理回應資料
         try:
-            result = response.json()["result"]
-            asset_urls = result["AssetURLs"]
+            result = response.json().get("result", {})
+            asset_urls = result.get("AssetURLs", [])
             if len(asset_urls) > 0:
                 m3u8_url_with_params = asset_urls[0]
                 # 使用 split() 函數分割字串，只取得 .m3u8 檔案本身的網址
@@ -43,8 +43,8 @@ def get_m3u8_url(channel_id):
                 return m3u8_url
             else:
                 return None
-        except KeyError:
-            print("回應資料中缺少必要的欄位 'result' 或 'AssetURLs'")
+        except Exception as e:
+            print("解析回應資料時發生錯誤：", e)
             return None
     else:
         # 請求失敗，處理錯誤訊息
